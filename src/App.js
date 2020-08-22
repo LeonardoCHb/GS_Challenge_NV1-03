57dff1b3d0652a986a71b72553df0f4ab3d78da2
LeonardoCHb <leonardohably@hotmail.com>
2020-08-22 17:54:48 -0300

    may god help us all

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
+import api from './services/api'
 
 import {
   SafeAreaView,
@@ -11,6 +12,14 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+      api.get('repositories').then(response => {
+          setRepositories(response.data)
+      })
+  }, [])
+
   async function handleLikeRepository(id) {
     // Implement "Like Repository" functionality
   }
